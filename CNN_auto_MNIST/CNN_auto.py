0df95c979419f6ac11c5ca19ab57186857c51543
CHIANGCHIUTI <sc510377@gmail.com>
2017-04-04 19:14:44 +0800

    add maxpooling layer and implment its correspond unpooing

--- CNN_auto_MNIST/CNN_auto.py
+++ CNN_auto_MNIST/CNN_auto.py
@@ -55,23 +55,24 @@ class CNN_auto:
 		'''
 		encode_layer_1 = self.conv2d(x, weights['conv1'], bias['conv1'])
 		encode_layer_1 = tf.nn.relu(encode_layer_1)
-		# encode_layer_1 = self.maxpool2d(encode_layer_1)
+		encode_layer_1, argmax_1 = self.maxpool2d(encode_layer_1)
 
 		encode_layer_2 = self.conv2d(encode_layer_1, weights['conv2'], bias['conv2'])
 		encode_layer_2 = tf.nn.relu(encode_layer_2)
-		# encode_layer_2 = self.maxpool2d(encode_layer_2)
+		encode_layer_2, argmax_2 = self.maxpool2d(encode_layer_2)
 
 		enocde_layer_output = encode_layer_2
-
-		output_shape_of_dconv1 = tf.pack([tf.shape(x)[0], 28, 28, self.decoder_1])  # output channel: 16
-		decode_layer_1 = self.deconv2d(enocde_layer_output, weights['deconv1'], bias['deconv1'], output_shape_of_dconv1)
+		print('enocde_layer_output shape {}'.format(enocde_layer_output.get_shape()))  # [, 7, 7, 32]
+		# max unpooling
+		decode_layer_1 = self.unmaxpool2d(enocde_layer_output, argmax_2)  # output [, 14, 14, 32]
+		output_shape_of_dconv1 = tf.pack([tf.shape(x)[0], 14, 14, self.decoder_1])  # output channel: 16
+		decode_layer_1 = self.deconv2d(decode_layer_1, weights['deconv1'], bias['deconv1'], output_shape_of_dconv1)
 		decode_layer_1 = tf.nn.relu(decode_layer_1)
-		# decode_layer_1 = max_unpool_2x2(decode_layer_1)
 
+		decode_layer_2 = self.unmaxpool2d(decode_layer_1, argmax_1)  # output [, 28, 28, 16]
 		output_shape_of_dconv2 = tf.pack([tf.shape(x)[0], 28, 28, self.decoder_2])  # output channel: 1
-		decode_layer_2 = self.deconv2d(decode_layer_1, weights['deconv2'], bias['deconv2'], output_shape_of_dconv2)
+		decode_layer_2 = self.deconv2d(decode_layer_2, weights['deconv2'], bias['deconv2'], output_shape_of_dconv2)
 		decode_layer_2 = tf.nn.tanh(decode_layer_2)
-		# decode_layer_2 = unmaxpool2d(decode_layer_2)
 
 		decode_layer_output = decode_layer_2
 		return enocde_layer_output, decode_layer_output
@@ -95,15 +96,41 @@ class CNN_auto:
 		return x
 
 	def maxpool2d(self, x, k=2):
+		_, argmax = tf.nn.max_pool_with_argmax(x, ksize=[1, k, k, 1], strides=[
+			1, k, k, 1], padding='SAME')
 		x = tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[
 			1, k, k, 1], padding='SAME')
-		return x
+		return x, argmax
 
-	def unmaxpool2d(self, raveled_argmax, output_shape):
-		def unravel_argmax(argmax, shape):
-			output_list = []
-			output_list.append(argmax // (shape[2] * shape[3]))
-		print('raveled_argmax shape{}'.format(raveled_argmax.get_shape()))
+	def unmaxpool2d(self, updates, argmax, ksize=[1, 2, 2, 1]):
+		'''
+			argmax indeics:
+				[b, y, x, f] -> ((b * height + y) * width + x) * channels + f
+		'''
+		argmax = tf.to_int32(argmax)
+		input_shape = tf.shape(updates, out_type=tf.int32)
+		# calculate output shape
+		output_shape = (
+			input_shape[0],
+			input_shape[1] * ksize[1],
+			input_shape[2] * ksize[2],
+			input_shape[3])
+
+		one_like_mask = tf.ones_like(argmax, dtype=tf.int32)
+		batch_shape = tf.pack([input_shape[0], 1, 1, 1])
+		batch_range = tf.reshape(tf.range(output_shape[0], dtype=tf.int32), shape=batch_shape)
+		b = one_like_mask * batch_range
+		y = argmax // (output_shape[2] * output_shape[3])
+		x = argmax % (output_shape[2] * output_shape[3]) // output_shape[3]  # % and // at the sa,e prcedence
+		feature_range = tf.range(output_shape[3], dtype=tf.int32)
+		f = one_like_mask * feature_range
+		updates_size = tf.size(updates)
+		indices = tf.transpose(tf.reshape(tf.stack([b, y, x, f]), [4, updates_size]))
+		print('indices shape {}'.format(indices.get_shape()))
+		values = tf.reshape(updates, [updates_size])  # faltturn
+		print('value shape {}'.format(values.get_shape()))
+		ret = tf.scatter_nd(indices, values, output_shape)
+		return ret
 
 	def max_unpool_2x2(x, output_shape):
 		out = tf.concat_v2([x, tf.zeros_like(x)], 3)
